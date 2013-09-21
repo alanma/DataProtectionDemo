72c58fedc3f2ca47122eb6d559d7c061db05149c
Erik Romijn <eromijn@solidlinks.nl>
2013-09-21 10:35:57 +0200

    Add keychain item stored with default accessibility.

--- DataProtectionDemo/DPDKeychainStorage.m
+++ DataProtectionDemo/DPDKeychainStorage.m
@@ -8,6 +8,7 @@
 
 #import "DPDKeychainStorage.h"
 
+NSString *const keychainAccountDefault = @"demo-default";
 // Note that "always" in keychain means "always available".
 NSString *const keychainAccountAlways = @"demo-always";
 NSString *const keychainAccountAfterFirstUnlock = @"demo-afterfirstunlock";
@@ -17,6 +18,9 @@ NSString *const keychainAccountWhenUnlocked = @"demo-whenunlocked";
 @implementation DPDKeychainStorage
 
 - (void)storeAllKeychainData:(NSData *)data {
+    [self storeKeychainItem:data
+                    account:keychainAccountDefault
+              accessibility:nil];
     [self storeKeychainItem:data
                     account:keychainAccountAlways
               accessibility:kSecAttrAccessibleAlways];
@@ -31,6 +35,7 @@ NSString *const keychainAccountWhenUnlocked = @"demo-whenunlocked";
 
 - (NSArray *)retrieveAllKeychainData {
     NSMutableArray *result = [NSMutableArray array];
+    [result addObject:[self retrieveKeychainItem:keychainAccountDefault]];
     [result addObject:[self retrieveKeychainItem:keychainAccountAlways]];
     [result addObject:[self retrieveKeychainItem:keychainAccountAfterFirstUnlock]];
     [result addObject:[self retrieveKeychainItem:keychainAccountWhenUnlocked]];
@@ -40,12 +45,14 @@ NSString *const keychainAccountWhenUnlocked = @"demo-whenunlocked";
 
 - (void)storeKeychainItem:(NSData *)data account:(NSString *)account accessibility:(CFTypeRef)accessibility {
     // Note that metadata, like the account name, is not encrypted.
-    NSDictionary *item = @{
+    NSMutableDictionary *item = [NSMutableDictionary dictionaryWithDictionary:@{
         (__bridge id)kSecAttrAccount: account,
         (__bridge id)kSecClass: (__bridge id)kSecClassGenericPassword,
-        (__bridge id)kSecAttrAccessible: (__bridge id)accessibility,
         (__bridge id)kSecValueData: data,
-    };
+    }];
+    if (accessibility) {
+        [item setObject:(__bridge id)accessibility forKey:(__bridge id)kSecAttrAccessible];
+    }
     
     OSStatus error = SecItemAdd((__bridge CFDictionaryRef)item, NULL);
     if(error) {
@@ -67,7 +74,7 @@ NSString *const keychainAccountWhenUnlocked = @"demo-whenunlocked";
     CFDataRef result = NULL;
     OSStatus error = SecItemCopyMatching((__bridge CFDictionaryRef)query, (CFTypeRef *)&result);
     if(error) {
-        NSLog(@"Failed to create Keychain item for %@: %d", account, (int)error);
+        NSLog(@"Failed to retrieve Keychain item for %@: %d", account, (int)error);
         return [NSData data];
     } else {
         NSLog(@"Retrieved Keychain item for %@", account);
